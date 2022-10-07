df3f18bc2563505306d0287d087d029e081a4ad6
kaoZ57 <57824756+kaoZ57@users.noreply.github.com>
2022-10-07 12:16:20 +0700

    main

--- Exercise-2/Program.cs
+++ Exercise-2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,43 @@ namespace Exercise_2
     {
         static void Main(string[] args)
         {
+            float cash = 100;
+            int seclct;
+
+            Console.WriteLine("Account");
+            Console.WriteLine("=======");
+            Console.WriteLine("1. Deposit");
+            Console.WriteLine("2. Withdraw");
+            Console.WriteLine("3. Check Balance");
+            Console.WriteLine("0. Exit");
+            Console.Write("Select number : ");
+        UP:
+            try
+            {
+                seclct = Int32.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                goto UP;
+            }
+
+            if (seclct == 1)
+            {
+
+            }
+            if (seclct == 2)
+            {
+
+            }
+            if (seclct == 3)
+            {
+
+            }
+            if (seclct == 0)
+            {
+
+            }
+
         }
     }
 }
